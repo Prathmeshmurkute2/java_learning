b9df265ddf8514809d03ddfc57368e3ad6f175ee
prathmeshmurkute2 <prathmeshmurkute983@gmail.com>
2026-06-28 21:03:31 +0530

    add ds and player

---
+++ Project_day4/src/Player.java
@@ -0,0 +1,26 @@
+
+public class Player {
+    private int playerId;
+    private String playerName;
+    private int age;
+    private int runs;
+
+    public Player(int playerId, String playerName, int age, int runs) {
+        this.playerId = playerId;
+        this.playerName = playerName;
+        this.age = age;
+        this.runs = runs;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    @Override
+    public String toString() {
+        return "Player ID : " + playerId +
+               "\nName : " + playerName +
+               "\nAge : " + age +
+               "\nRuns : " + runs;
+    }
+}
